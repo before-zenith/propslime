d47ba6eee18594be2dc0c2843be17d7db8462f03
before-zenith <cplusplus.fanboy@gmail.com>
2026-04-03 10:55:04 +0700

    Initial commit

---
+++ src/client/java/net/zenith/propslime/client/PropDevSlimeClient.java
@@ -0,0 +1,10 @@
+package net.zenith.propslime.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PropDevSlimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/zenith/propslime/client/PropDevSlimeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zenith.propslime.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PropDevSlimeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
